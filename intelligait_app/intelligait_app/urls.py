83b7801d9498c66c01fdc8de2c0b75f853748f6b
Candice Smith <candice.smith4@griffithuni.edu.au>
2021-10-01 19:39:53 +1000

    Updated css and html, change to user model

--- intelligait_app/intelligait_app/urls.py
+++ intelligait_app/intelligait_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf.urls import url, include
+from django.conf.urls import include
 from users import views as users_views
 from django.conf import settings
 from django.conf.urls.static import static
--- intelligait_app/users/views.py
+++ intelligait_app/users/views.py
@@ -36,7 +36,6 @@ def profile(request):
         'u_form': u_form,
         'p_form': p_form
     }
-
     return render(request, 'users/profile.html', context)
 
 """
